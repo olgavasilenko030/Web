74501d6314902eaae9e13f865a6f8f04151b8594
Vasil_th18@top-academy.ru <Vasil_th18@top-academy.ru>
2025-07-22 23:51:29 +0500

    done

---
+++ 02-JavaScript/03-DOM/DOM.js
@@ -0,0 +1,10 @@
+﻿// JavaScript source code
+let check = document.getElementById("check");
+check.innerHTML = "Hello DOM";
+console.log(check.outerHTML);
+
+let configuration = document.getElementById("configuration");
+configuration.append("append();");
+configuration.prepend("prepend();");
+configuration.before("<h2>Добавление элементов к ноде</h2>");
+configuration.after("Вот и сказочке конец");
